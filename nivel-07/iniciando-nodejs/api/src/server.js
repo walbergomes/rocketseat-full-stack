9c9f3ae8c785f3656fc55c16880f86c35bdc5030
Walber <walberg.s48@gmail.com>
2024-11-21 20:57:33 -0300

    Recuperando dados no body

--- nivel-07/iniciando-nodejs/api/src/server.js
+++ nivel-07/iniciando-nodejs/api/src/server.js
@@ -1,6 +1,6 @@
 import http from "node:http"
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request
 
   if(method === "GET" && url === "/products") {
@@ -8,6 +8,15 @@ const server = http.createServer((request, response) => {
   }
 
   if(method === "POST" && url === "/products") {
+    const buffers = []
+
+    for await (const chunk of request) {
+      buffers.push(chunk)
+    }
+
+    // console.log(buffers)
+    console.log(Buffer.concat(buffers).toString())
+
     return response.writeHead(201).end("Produto Cadastrado")
   }
 
