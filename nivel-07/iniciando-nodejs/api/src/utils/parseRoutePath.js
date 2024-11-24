314da571d278343c19971cc664f8c031ea797fc6
Walber <walberg.s48@gmail.com>
2024-11-24 16:20:25 -0300

    obtendo parametros nomeados

--- nivel-07/iniciando-nodejs/api/src/utils/parseRoutePath.js
+++ nivel-07/iniciando-nodejs/api/src/utils/parseRoutePath.js
@@ -3,7 +3,7 @@ export function parseRoutePath(path) {
 
   const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9-_]+)")
 
-  const pathRegex = new RegExp(params)
+  const pathRegex = new RegExp(`${params}(?<query>\\?(.*))?$`)
 
   return pathRegex
 }
