4729bb70a793f7dd20fcc636019615b21ac25c82
Walber <walberg.s48@gmail.com>
2024-11-26 21:57:38 -0300

    salvando dados no arquivo

--- nivel-07/iniciando-nodejs/api/src/database.js
+++ nivel-07/iniciando-nodejs/api/src/database.js
@@ -1,33 +1,33 @@
-import fs from "node:fs/promises"
+import fs from "node:fs/promises";
 
-const DATABASE_PATH = new URL("db.json", import.meta.url) 
+const DATABASE_PATH = new URL("db.json", import.meta.url);
 
 export class Database {
-  database = {}
+  database = {};
 
   constructor() {
-    this.persist()
+    fs.readFile(DATABASE_PATH, "utf-8")
+      .then((data) => {
+        this.database = JSON.parse(data);
+      })
+      .catch(() => this.persist());
   }
 
   persist() {
-    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database));
   }
 
-   insert(table, data) {
-
-    if(Array.isArray(this.database[table])) {
-      this.database[table].push(data)
+  insert(table, data) {
+    if (Array.isArray(this.database[table])) {
+      this.database[table].push(data);
     } else {
-      this.database[table] = [data]
+      this.database[table] = [data];
     }
 
-    this.persist()
-
+    this.persist();
   }
 
   select(table) {
-    return this.database[table]
+    return this.database[table];
   }
-
-
 }
