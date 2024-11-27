8c1cb8254dd0b25cae9551753cd704dcd96d1059
Walber <walberg.s48@gmail.com>
2024-11-26 21:30:06 -0300

    salvando dados em memoria

---
+++ nivel-07/iniciando-nodejs/api/src/database.js
@@ -0,0 +1,19 @@
+export class Database {
+  database = {}
+
+  insert(table, data) {
+
+    if(Array.isArray(this.database[table])) {
+      this.database[table].push(data)
+    } else {
+      this.database[table] = [data]
+    }
+
+  }
+
+  select(table) {
+    return this.database[table]
+  }
+
+
+}
--- nivel-07/iniciando-nodejs/api/src/middlewares/routeHandler.js
+++ nivel-07/iniciando-nodejs/api/src/middlewares/routeHandler.js
@@ -1,5 +1,8 @@
 import { routes } from "../routes.js";
 import { extractQueryParams } from "../utils/extract-query-params.js";
+import { Database } from "../database.js";
+
+const database = new Database()
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
@@ -11,12 +14,10 @@ export function routeHandler(request, response) {
 
     const { query, ...params } = routeParams.groups
 
-    extractQueryParams(query)
-
     request.params = params
     request.query = query ? extractQueryParams(query) : {}
 
-    return route.controller(request, response)
+    return route.controller({ request, response, database })
   }
 
   return response.writeHead(404).end("Rota não encontrada");
