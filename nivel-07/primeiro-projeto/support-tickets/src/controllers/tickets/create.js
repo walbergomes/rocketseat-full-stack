1e0f8327b76e39f86dc7d9fcfdb8983b4fd90a0d
Walber <walberg.s48@gmail.com>
2024-12-01 16:49:43 -0300

    salvando o ticket

--- nivel-07/primeiro-projeto/support-tickets/src/controllers/tickets/create.js
+++ nivel-07/primeiro-projeto/support-tickets/src/controllers/tickets/create.js
@@ -1,6 +1,6 @@
 import { randomUUID } from "node:crypto"
 
-export function create({ request, response }) {
+export function create({ request, response, database }) {
   const { equipament, description, user_name } = request.body
 
   const ticket = {
@@ -13,6 +13,7 @@ export function create({ request, response }) {
     updated_at: new Date()
   }
 
+  database.insert("tickets", ticket)
 
-  return response.end(JSON.stringify(ticket))
+  return response.writeHead(201).end(JSON.stringify(ticket))
 }
