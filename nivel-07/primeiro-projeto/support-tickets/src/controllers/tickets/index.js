320972196f3d24e339e93283a278dd2d1b6eb939
Walber <walberg.s48@gmail.com>
2024-12-03 22:33:37 -0300

    obtendo parametros das rotas

--- nivel-07/primeiro-projeto/support-tickets/src/controllers/tickets/index.js
+++ nivel-07/primeiro-projeto/support-tickets/src/controllers/tickets/index.js
@@ -1,4 +1,7 @@
 export function index({ request, response, database }) {
+  const { status } = request.query
+  console.log(status)
+
   const tickets = database.select("tickets")
 
   return response.end(JSON.stringify(tickets))
