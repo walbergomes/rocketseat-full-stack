404bce64d236654956d38c5ab995934ba6f764da
Walber <walberg.s48@gmail.com>
2024-12-05 19:53:27 -0300

    rota pra atualizar ticket

--- nivel-07/primeiro-projeto/support-tickets/src/routes/tickets.js
+++ nivel-07/primeiro-projeto/support-tickets/src/routes/tickets.js
@@ -1,5 +1,6 @@
 import { create } from "../controllers/tickets/create.js"
 import { index } from "../controllers/tickets/index.js"
+import { update } from "../controllers/tickets/update.js"
 
 
 export const tickets = [
@@ -12,5 +13,10 @@ export const tickets = [
     method: "GET",
     path: "/tickets",
     controller: index,
+  },
+  {
+    method: "PUT",
+    path: "/tickets/:id",
+    controller: update,
   }
 ]
--- nivel-07/primeiro-projeto/support-tickets/src/utils/parseRoutePath.js
+++ nivel-07/primeiro-projeto/support-tickets/src/utils/parseRoutePath.js
@@ -1,7 +1,7 @@
 export function parseRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
 
-  const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9_]+)")
+  const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9-_]+)")
 
   const pathRegex = new RegExp(`^${params}(?<query>\\?(.*))?$`)
 
