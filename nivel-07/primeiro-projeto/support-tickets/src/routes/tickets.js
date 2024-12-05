f7d50bb20b3f90492dbe675102e8bc4360de06eb
Walber <walberg.s48@gmail.com>
2024-12-05 20:39:51 -0300

    removendo um ticket

--- nivel-07/primeiro-projeto/support-tickets/src/routes/tickets.js
+++ nivel-07/primeiro-projeto/support-tickets/src/routes/tickets.js
@@ -2,6 +2,7 @@ import { create } from "../controllers/tickets/create.js"
 import { index } from "../controllers/tickets/index.js"
 import { update } from "../controllers/tickets/update.js"
 import { updateStatus } from "../controllers/tickets/updateStatus.js"
+import { remove } from "../controllers/tickets/remove.js"
 
 
 export const tickets = [
@@ -24,5 +25,10 @@ export const tickets = [
     method: "PATCH",
     path: "/tickets/:id/close",
     controller: updateStatus,
+  },
+  {
+    method: "DELETE",
+    path: "/tickets/:id",
+    controller: remove,
   }
 ]
