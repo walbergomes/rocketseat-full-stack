9ed9a5be87539ab0d70da2e29175ee3cffea900e
Walber <walberg.s48@gmail.com>
2024-12-05 20:14:12 -0300

    atualizando o ticket

--- nivel-07/primeiro-projeto/support-tickets/src/controllers/tickets/update.js
+++ nivel-07/primeiro-projeto/support-tickets/src/controllers/tickets/update.js
@@ -1,7 +1,13 @@
 export function update({ request, response, database }) {
   const { id } = request.params
   const { equipament, description } = request.body
-  
+
+
+  database.update("tickets", id, {
+    equipament,
+    description,
+    update_at: new Date()
+  })
 
   return response.end()
 }
