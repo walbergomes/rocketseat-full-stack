f6a36b46af55e4fc64633bd6abd32b8b039624c5
Walber <walberg.s48@gmail.com>
2024-12-05 19:31:16 -0300

    filtrando pelo status

--- nivel-07/primeiro-projeto/support-tickets/src/controllers/tickets/index.js
+++ nivel-07/primeiro-projeto/support-tickets/src/controllers/tickets/index.js
@@ -1,8 +1,9 @@
 export function index({ request, response, database }) {
   const { status } = request.query
-  console.log(status)
 
-  const tickets = database.select("tickets")
+  const filters = status ? { status } : null
+
+  const tickets = database.select("tickets", filters)
 
   return response.end(JSON.stringify(tickets))
 }
